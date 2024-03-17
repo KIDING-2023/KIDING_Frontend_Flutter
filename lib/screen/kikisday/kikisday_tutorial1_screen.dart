9be561d0b6b874f29ae25c43e007d0e555699306
Siwon Jeon <siwonjeon234@gmail.com>
2024-03-17 14:57:30 +0900

    UPDATE: 키키의 하루 튜토리얼 화면 디자인 수정

--- lib/screen/kikisday/kikisday_tutorial1_screen.dart
+++ lib/screen/kikisday/kikisday_tutorial1_screen.dart
@@ -10,7 +10,8 @@ void main() {
 
 class KikisdayTutorial1Screen extends StatefulWidget {
   @override
-  _KikisdayTutorial1ScreenState createState() => _KikisdayTutorial1ScreenState();
+  _KikisdayTutorial1ScreenState createState() =>
+      _KikisdayTutorial1ScreenState();
 }
 
 class _KikisdayTutorial1ScreenState extends State<KikisdayTutorial1Screen> {
@@ -18,60 +19,64 @@ class _KikisdayTutorial1ScreenState extends State<KikisdayTutorial1Screen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/kikisday/kikisday_tutorial1_bg.png'),
-            fit: BoxFit.cover,
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image:
+                        AssetImage('assets/kikisday/kikisday_tutorial1_bg.png'),
+                    fit: BoxFit.cover)),
           ),
-        ),
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(top: 45.0, left: 30.0, right: 30.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  GestureDetector(
-                    onTap: () => Navigator.pop(context),
-                    child: Image.asset('assets/kikisday/kikisday_back_btn.png', width: 13.16, height: 20.0),
-                  ),
-                  Consumer<TimerModel>( // TimerModel의 현재 시간을 소비합니다.
-                    builder: (context, timer, child) => Text(
-                      timer.formattedTime, // TimerModel로부터 현재 시간을 가져옵니다.
-                      style: TextStyle(
-                        fontFamily: 'Nanum',
-                        fontSize: 15,
-                        color: Color(0xFF868686),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Image.asset('assets/kikisday/kikisday_tutorial1_text.png', width: 339.79, height: 296.58),
-                    Image.asset('assets/kikisday/kikisday_tutorial1_ch.png', width: 360, height: 348.39),
-                    GestureDetector(
-                      onTap: () {
-                        // 튜토리얼2로 이동
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => KikisdayTutorial2Screen()),
-                        );
-                      },
-                      child: Image.asset('assets/kikisday/kikisday_ok_btn.png', width: 322.07, height: 44.75),
-                    ),
-                  ],
+          Padding(
+            padding: const EdgeInsets.only(top: 45.0, left: 30.0, right: 30.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () => Navigator.pop(context),
+                  child: Image.asset('assets/kikisday/kikisday_back_btn.png',
+                      width: 13.16, height: 20.0),
                 ),
-              ),
+                Consumer<TimerModel>(
+                  // TimerModel의 현재 시간을 소비합니다.
+                  builder: (context, timer, child) => Text(
+                    timer.formattedTime, // TimerModel로부터 현재 시간을 가져옵니다.
+                    style: TextStyle(
+                      fontFamily: 'Nanum',
+                      fontSize: 15,
+                      color: Color(0xFF868686),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+          Positioned(
+              left: 10.11,
+              top: 125.22,
+              child: Image.asset('assets/kikisday/kikisday_tutorial1_text.png',
+                  width: 339.79, height: 296.58)),
+          Positioned(
+              top: 346.61,
+              child: Image.asset('assets/kikisday/kikisday_tutorial1_ch.png',
+                  width: 360, height: 348.39)),
+          Positioned(
+              left: 19.37,
+              top: 695,
+              child: GestureDetector(
+                child: Image.asset('assets/kikisday/kikisday_ok_btn.png',
+                    width: 322.07, height: 44.75),
+                onTap: () {
+                  // 튜토리얼2로 이동
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => KikisdayTutorial2Screen()),
+                  );
+                },
+              )),
+        ],
       ),
     );
   }
--- lib/screen/kikisday/kikisday_tutorial2_screen.dart
+++ lib/screen/kikisday/kikisday_tutorial2_screen.dart
@@ -14,60 +14,64 @@ class _KikisdayTutorial2ScreenState extends State<KikisdayTutorial2Screen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/kikisday/kikisday_tutorial1_bg.png'),
-            fit: BoxFit.cover,
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image:
+                    AssetImage('assets/kikisday/kikisday_tutorial1_bg.png'),
+                    fit: BoxFit.cover)),
           ),
-        ),
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(top: 45.0, left: 30.0, right: 30.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  GestureDetector(
-                    onTap: () => Navigator.pop(context),
-                    child: Image.asset('assets/kikisday/kikisday_back_btn.png', width: 13.16, height: 20.0),
-                  ),
-                  Consumer<TimerModel>( // TimerModel의 현재 시간을 소비합니다.
-                    builder: (context, timer, child) => Text(
-                      timer.formattedTime, // TimerModel로부터 현재 시간을 가져옵니다.
-                      style: TextStyle(
-                        fontFamily: 'Nanum',
-                        fontSize: 15,
-                        color: Color(0xFF868686),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Image.asset('assets/kikisday/kikisday_tutorial2_text.png', width: 339.79, height: 229.08),
-                    Image.asset('assets/kikisday/kikisday_tutorial2_ch.png', width: 360, height: 302.53),
-                    GestureDetector(
-                      onTap: () {
-                        // 주사위 화면으로 이동
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => KikisdayDiceScreen()),
-                        );
-                      },
-                      child: Image.asset('assets/kikisday/kikisday_ok_btn.png', width: 322.07, height: 44.75),
-                    ),
-                  ],
+          Padding(
+            padding: const EdgeInsets.only(top: 45.0, left: 30.0, right: 30.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () => Navigator.pop(context),
+                  child: Image.asset('assets/kikisday/kikisday_back_btn.png',
+                      width: 13.16, height: 20.0),
                 ),
-              ),
+                Consumer<TimerModel>(
+                  // TimerModel의 현재 시간을 소비합니다.
+                  builder: (context, timer, child) => Text(
+                    timer.formattedTime, // TimerModel로부터 현재 시간을 가져옵니다.
+                    style: TextStyle(
+                      fontFamily: 'Nanum',
+                      fontSize: 15,
+                      color: Color(0xFF868686),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+          Positioned(
+              left: 10.11,
+              top: 125.22,
+              child: Image.asset('assets/kikisday/kikisday_tutorial2_text.png',
+                  width: 339.79, height: 229.08)),
+          Positioned(
+              top: 370.14,
+              child: Image.asset('assets/kikisday/kikisday_tutorial2_ch.png',
+                  width: 360, height: 302.53)),
+          Positioned(
+              left: 19.37,
+              top: 695,
+              child: GestureDetector(
+                child: Image.asset('assets/kikisday/kikisday_ok_btn.png',
+                    width: 322.07, height: 44.75),
+                onTap: () {
+                  // 튜토리얼2로 이동
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => KikisdayDiceScreen()),
+                  );
+                },
+              )),
+        ],
       ),
     );
   }
