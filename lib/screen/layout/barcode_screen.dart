ca9b52ffb770bff4900777a802dda708b779985d
jeonsiwon <siwonjeon234@gmail.com>
2024-08-14 19:29:32 +0900

    UPDATE: 바코드 스캔 큐알코드 스캔으로 변경

--- lib/screen/layout/barcode_screen.dart
+++ lib/screen/layout/barcode_screen.dart
@@ -1,14 +1,14 @@
 import 'dart:developer';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_qr_bar_scanner/qr_bar_scanner_camera.dart';
-import 'package:kiding/screen/space/space_earth_complete_screen.dart';
-import 'package:kiding/screen/space/space_mars_complete_screen.dart';
-import 'package:kiding/screen/space/space_saturn_complete_screen.dart';
-import 'package:kiding/screen/space/space_venus_complete_screen.dart';
-import 'package:mysql_client/mysql_protocol.dart';
+import 'package:google_ml_kit/google_ml_kit.dart' as mlkit;
+import 'package:image_picker/image_picker.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 import 'exit_layout.dart';
+
+import 'dart:io' as io;
 
 class BarcodeScreen extends StatefulWidget {
   final int currentNumber;
@@ -22,133 +22,155 @@ class BarcodeScreen extends StatefulWidget {
 }
 
 class _BarcodeScreenState extends State<BarcodeScreen> {
-  String? _qrInfo = 'Scan a QR/Bar code';
-  bool _camState = false;
+  Barcode? result;
+  QRViewController? controller;
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
 
-  // 다음 화면
-  late var nextScreen;
-
-  _qrCallback(String? code) {
-    setState(() {
-      _camState = false;
-      _qrInfo = code;
-    });
-
-    // 3초의 딜레이 후 다음 화면으로 전환
-    Future.delayed(Duration(seconds: 3), () {
-      _navigateToNextScreen();
-    });
-  }
-
-  _scanCode() {
-    setState(() {
-      _camState = true;
-    });
-  }
+  bool useCamera = true;
+  final picker = ImagePicker();
 
   @override
-  void initState() {
-    super.initState();
-    _scanCode();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  void _navigateToNextScreen() {
-    // // 각 카드덱의 답변 완료 화면으로 이동
-    // if (widget.currentNumber <= 3) {
-    //   nextScreen =
-    //       SpaceEarthCompleteScreen(currentNumber: widget.currentNumber);
-    // } else if (widget.currentNumber >= 4 && widget.currentNumber <= 6) {
-    //   nextScreen =
-    //       SpaceVenusCompleteScreen(currentNumber: widget.currentNumber);
-    // } else if (widget.currentNumber >= 7 && widget.currentNumber <= 9) {
-    //   nextScreen = SpaceMarsCompleteScreen(currentNumber: widget.currentNumber);
-    // } else {
-    //   nextScreen =
-    //       SpaceSaturnCompleteScreen(currentNumber: widget.currentNumber);
-    // }
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(builder: (context) => widget.completeScreen),
-    );
-    log('currentNumber: ${widget.currentNumber}');
+  void reassemble() {
+    super.reassemble();
+    if (io.Platform.isAndroid) {
+      controller!.pauseCamera();
+    } else if (io.Platform.isIOS) {
+      controller!.resumeCamera();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Color(0xFF363E7C),
-        child: Stack(
-          alignment: Alignment.center,
-          children: <Widget>[
-            _camState
-                ? Center(
-                    child: SizedBox(
-                    width: 250,
-                    height: 150,
-                    child: QRBarScannerCamera(
-                      onError: (context, error) => Text(
-                        error.toString(),
-                        style: TextStyle(color: Colors.red),
-                      ),
-                      qrCodeCallback: (code) {
-                        _qrCallback(code);
-                      },
-                    ),
-                  ))
-                : Positioned(bottom: 200, child: Text(_qrInfo!)),
-            Positioned(
-              top: 220,
-              left: 0,
-              right: 0,
-              child: RichText(
-                textAlign: TextAlign.center, // 텍스트 정렬
-                text: TextSpan(
-                  style: TextStyle(
-                    fontSize: 25,
-                    color: Colors.white, // 기본 텍스트 색상
-                  ),
-                  children: <TextSpan>[
-                    TextSpan(
-                      text: '카드덱의\n',
-                      style: TextStyle(fontFamily: 'Nanum'), // 두께 굵게
-                    ),
-                    TextSpan(
-                      text: '바코드',
-                      style: TextStyle(
-                          fontFamily: 'Nanum', color: Color(0xFFFF8A5B)),
-                    ),
-                    TextSpan(
-                        text: '를 인식해주세요',
-                        style: TextStyle(fontFamily: 'NanumRegular')),
-                  ],
+      body: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          _buildQrView(context),
+          Positioned(
+            top: 200,
+            left: 0,
+            right: 0,
+            child: RichText(
+              textAlign: TextAlign.center, // 텍스트 정렬
+              text: TextSpan(
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white, // 기본 텍스트 색상
                 ),
+                children: <TextSpan>[
+                  TextSpan(
+                    text: '카드덱의\n',
+                    style: TextStyle(fontFamily: 'Nanum'), // 두께 굵게
+                  ),
+                  TextSpan(
+                    text: '바코드',
+                    style: TextStyle(
+                        fontFamily: 'Nanum', color: Color(0xFFFF8A5B)),
+                  ),
+                  TextSpan(
+                      text: '를 인식해주세요',
+                      style: TextStyle(fontFamily: 'NanumRegular')),
+                ],
               ),
             ),
+          ),
+          if (result != null)
             Positioned(
-              top: 30,
-              left: 15,
-              child: IconButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => ExitLayout()),
-                    );
-                  },
-                  icon: Image.asset(
-                    'assets/space/back_icon_white.png',
-                    width: 13.16,
-                    height: 20.0,
-                  )),
+              bottom: 200,
+              child: Text(
+                'Barcode Type: ${describeEnum(result!.format)}   Data: ${result!.code}',
+                style: TextStyle(
+                    fontSize: 15,
+                    fontFamily: 'NanumRegular',
+                    color: Colors.white),
+              ),
             ),
-          ],
-        ),
+          Positioned(
+            bottom: 200,
+            child: Text(
+              result == null ? 'Scan a code' : '',
+              style: TextStyle(
+                  fontSize: 15,
+                  fontFamily: 'NanumRegular',
+                  color: Colors.white),
+            ),
+          ),
+          Positioned(
+            top: 30,
+            left: 15,
+            child: IconButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ExitLayout()),
+                  );
+                },
+                icon: Image.asset(
+                  'assets/kikisday/back_icon.png',
+                  width: 13.16,
+                  height: 20.0,
+                )),
+          ),
+        ],
       ),
+    );
+  }
+
+  Widget _buildQrView(BuildContext context) {
+    var scanArea = 250.0;
+    // To ensure the Scanner view is properly sizes after rotation
+    // we need to listen for Flutter SizeChanged notification and update controller
+    return QRView(
+      key: qrKey,
+      onQRViewCreated: _onQRViewCreated,
+      overlay: QrScannerOverlayShape(
+          borderColor: Color(0xFFFF8A5B),
+          borderRadius: 10,
+          borderLength: 50,
+          borderWidth: 20,
+          cutOutSize: scanArea),
+      onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    setState(() {
+      this.controller = controller;
+    });
+
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData;
+      });
+      if (result != null) {
+        _navigateToNextScreen();
+      }
+    });
+  }
+
+  void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
+    log('${DateTime.now().toIso8601String()}_onPermissionSet $p');
+    if (!p) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('no Permission')),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  void _navigateToNextScreen() {
+    setState(() {
+      controller?.pauseCamera();
+      controller?.dispose();
+    });
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => widget.completeScreen),
     );
   }
 }
