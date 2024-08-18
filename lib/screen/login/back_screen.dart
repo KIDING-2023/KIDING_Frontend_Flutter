0b7b2b7e9b6a9007b82fdb858597548b927021a9
jeonsiwon <siwonjeon234@gmail.com>
2024-08-18 17:59:02 +0900

    UPDATE: 회원가입 뒤로가기 버튼 화면 비율 수정

--- lib/screen/login/back_screen.dart
+++ lib/screen/login/back_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:kiding/screen/login/find_nickname_screen.dart';
 import 'package:kiding/screen/login/start_screen.dart';
 
+// 회원가입 과정에서 뒤로 가기 버튼 클릭 시 표시할 화면
 class BackScreen extends StatefulWidget {
   const BackScreen({super.key});
 
@@ -12,66 +12,74 @@ class BackScreen extends StatefulWidget {
 class _BackScreenState extends State<BackScreen> {
   @override
   Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size; // 기기 화면 크기
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Image.asset(
-            'assets/login/back_screen_text.png',
-            width: 267.47,
-            height: 88,
-          ),
-          // 닉네임 찾기 & 비밀번호 찾기 버튼
-          Stack(
-            children: [
-              // 배경 이미지
-              Center(
-                  child: Image.asset(
-                'assets/login/back_btn_bg.png',
-                width: 259.96,
-                height: 120.99,
-              )),
-              // 버튼들을 세로로 배치하기 위한 Column
-              Center(
-                child: Container(
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 12),
-                    child: Column(
-                      mainAxisAlignment:
-                          MainAxisAlignment.spaceBetween, // 버튼들 사이의 공간을 균등하게 배분
-                      children: [
-                        // 처음 화면으로 버튼
-                        IconButton(
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => StartScreen()));
-                          },
-                          padding: EdgeInsets.zero,
-                          icon: Image.asset('assets/login/to_start_btn.png',
-                              width: 236.33, height: 43.63),
-                        ),
-                        // 닉네임 찾기 버튼
-                        IconButton(
-                          onPressed: () => Navigator.pop(context),
-                          padding: EdgeInsets.zero,
-                          icon: Image.asset(
-                              'assets/login/keep_signup_btn.png',
-                              width: 236.33,
-                              height: 43.63),
-                        )
-                      ],
+      body: Container(
+        color: Colors.white,
+        child: Stack(
+          children: [
+            // '회원가입을 진행중입니다' 텍스트
+            Positioned(
+              top: screenSize.height * 0.13,
+              left: 0,
+              right: 0,
+              child: Image.asset(
+                'assets/login/back_screen_text.png',
+                width: screenSize.width * 0.74,
+                height: screenSize.height * 0.11,
+              ),
+            ),
+            // 중단 or 계속 버튼
+            Positioned(
+              top: screenSize.height * 0.44,
+              left: 0,
+              right: 0,
+              child: Stack(
+                children: [
+                  // 버튼 배경 이미지
+                  Center(
+                      child: Image.asset(
+                        'assets/login/back_btn_bg.png',
+                        width: screenSize.width * 0.72,
+                        height: screenSize.height * 0.15,
+                      )),
+                  // 버튼 세로 배치
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 12.46, bottom: 12.46),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          // 중단 버튼
+                          IconButton(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => StartScreen()));
+                            },
+                            padding: EdgeInsets.zero,
+                            icon: Image.asset('assets/login/to_start_btn.png',
+                                width: screenSize.width * 0.66,
+                                height: screenSize.height * 0.05),
+                          ),
+                          // 계속 버튼
+                          IconButton(
+                            onPressed: () => Navigator.pop(context),
+                            padding: EdgeInsets.zero,
+                            icon: Image.asset('assets/login/keep_signup_btn.png',
+                                width: screenSize.width * 0.66,
+                                height: screenSize.height * 0.05),
+                          )
+                        ],
+                      ),
                     ),
                   ),
-                ),
+                ],
               ),
-            ],
-          ),
-          SizedBox(
-            height: 60,
-          )
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
