aee2333b641a2c68e303d305583234fd30e97583
siiion <siwonjeon234@gmail.com>
2024-10-19 00:08:42 +0900

    Feat: 회원가입 서버 연결

--- lib/screen/login/login_splash_screen.dart
+++ lib/screen/login/login_splash_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'choose_character_screen.dart';
 
 // '회원가입이 완료되었습니다.' 화면
@@ -13,13 +14,32 @@ class LoginSplashScreen extends StatefulWidget {
 }
 
 class _LoginSplashScreenState extends State<LoginSplashScreen> {
+  // Flutter Secure Storage 인스턴스 생성
+  final storage = FlutterSecureStorage();
+  String? token; // 불러온 토큰 저장할 변수
+
   @override
   void initState() {
     super.initState();
+    _loadToken();  // 토큰 불러오기
+  }
+
+  // 토큰을 불러오는 메서드
+  Future<void> _loadToken() async {
+    token = await storage.read(key: 'accessToken'); // 저장된 토큰 불러오기
+    if (token != null) {
+      print("Token: $token");  // 토큰이 정상적으로 불러와졌는지 확인
+    } else {
+      print("토큰을 찾을 수 없습니다.");
+    }
+
+    // 토큰을 불러온 후 화면 전환 (2초 지연)
     Timer(Duration(seconds: 2), () {
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(
-          builder: (_) => ChooseCharacterScreen(nickname: widget.nickname,
+          builder: (_) => ChooseCharacterScreen(
+            nickname: widget.nickname, // 닉네임 전달
+          ),
         ),
       );
     });
@@ -38,12 +58,11 @@ class _LoginSplashScreenState extends State<LoginSplashScreen> {
               Text(
                 '회원가입이 완료되었습니다',
                 style: TextStyle(
-                  fontFamily: 'nanum',
-                  fontSize: 20,
-                  color: Color(0xFF838383)
+                    fontFamily: 'nanum',
+                    fontSize: 20,
+                    color: Color(0xFF838383)
                 ),
-              )
-              // 여기에 추가 UI 구성 요소가 위치할 수 있습니다.
+              ),
             ],
           ),
         ),
--- lib/screen/login/password_confirm_screen.dart
+++ lib/screen/login/password_confirm_screen.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
+import 'package:kiding/constants/api_constants.dart';
 import 'back_screen.dart';
 import 'login_splash_screen.dart';
 
@@ -13,9 +14,9 @@ class PasswordConfirmScreen extends StatefulWidget {
 
   const PasswordConfirmScreen(
       {super.key,
-      required this.nickname,
-      required this.phoneNumber,
-      required this.password});
+        required this.nickname,
+        required this.phoneNumber,
+        required this.password});
 
   @override
   State<PasswordConfirmScreen> createState() => _PasswordConfirmScreenState();
@@ -23,134 +24,137 @@ class PasswordConfirmScreen extends StatefulWidget {
 
 class _PasswordConfirmScreenState extends State<PasswordConfirmScreen> {
   final TextEditingController _pwController =
-      TextEditingController(); // 비밀번호 입력 컨트롤러
+  TextEditingController(); // 비밀번호 입력 컨트롤러
   String errorMessage = "비밀번호를 다시 입력하세요"; // 에러 메시지 (항상 표시)
   bool _isErrorVisible = true; // 에러 메시지 앞 동그라미 가시성
   bool _isLoading = false; // 로딩 상태 표시
+
+  // Flutter Secure Storage 인스턴스 생성
+  final storage = FlutterSecureStorage();
 
   @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size; // 기기 화면 크기
     return Scaffold(
         body: Container(
-      color: Colors.white,
-      child: Stack(
-        children: [
-          // 뒤로가기 버튼
-          Positioned(
-            top: screenSize.height * 0.06,
-            left: screenSize.width * 0.03,
-            child: IconButton(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => BackScreen()));
-              },
-              icon: Image.asset('assets/kikisday/back_icon.png',
-                  width: screenSize.width * 0.04,
-                  height: screenSize.height * 0.03),
-            ),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          color: Colors.white,
+          child: Stack(
             children: [
-              // 안내문
-              Image.asset('assets/login/password_confirm_text.png',
-                  width: screenSize.width * 0.74,
-                  height: screenSize.height * 0.16),
-              // 비밀번호 입력칸, 입력 완료 버튼
+              // 뒤로가기 버튼
+              Positioned(
+                top: screenSize.height * 0.06,
+                left: screenSize.width * 0.03,
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => BackScreen()));
+                  },
+                  icon: Image.asset('assets/kikisday/back_icon.png',
+                      width: screenSize.width * 0.04,
+                      height: screenSize.height * 0.03),
+                ),
+              ),
               Column(
-                // 비밀번호 입력 칸
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Container(
-                    width: screenSize.width * 0.73,
-                    height: screenSize.height * 0.06,
-                    child: TextField(
-                      controller: _pwController,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          hintText: '비밀번호를 다시 입력하세요',
-                          hintStyle: TextStyle(color: Color(0xFFAAAAAA)),
-                          filled: true,
-                          fillColor: Color(0xfff6f6f6),
-                          enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide.none,
-                              borderRadius: BorderRadius.circular(100)),
-                          contentPadding: EdgeInsets.all(20)),
-                      style: TextStyle(
-                        fontFamily: 'nanum',
-                        fontSize: 17,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: screenSize.height * 0.01,
-                  ),
-                  // 비밀번호 오류 메시지
-                  Row(
+                  // 안내문
+                  Image.asset('assets/login/password_confirm_text.png',
+                      width: screenSize.width * 0.74,
+                      height: screenSize.height * 0.16),
+                  // 비밀번호 입력칸, 입력 완료 버튼
+                  Column(
+                    // 비밀번호 입력 칸
                     children: [
-                      Padding(
-                          padding:
-                              EdgeInsets.only(left: screenSize.width * 0.21)),
-                      Visibility(
-                        visible: _isErrorVisible,
-                        child: Icon(
-                          Icons.circle,
-                          size: screenSize.width * 0.01,
-                          fill: 1.0,
-                          color: Color(0xFFFFA37C),
+                      Container(
+                        width: screenSize.width * 0.73,
+                        height: screenSize.height * 0.06,
+                        child: TextField(
+                          controller: _pwController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                              hintText: '비밀번호를 다시 입력하세요',
+                              hintStyle: TextStyle(color: Color(0xFFAAAAAA)),
+                              filled: true,
+                              fillColor: Color(0xfff6f6f6),
+                              enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius: BorderRadius.circular(100)),
+                              contentPadding: EdgeInsets.all(20)),
+                          style: TextStyle(
+                            fontFamily: 'nanum',
+                            fontSize: 17,
+                            color: Colors.black,
+                          ),
                         ),
                       ),
-                      Padding(
-                          padding:
+                      SizedBox(
+                        height: screenSize.height * 0.01,
+                      ),
+                      // 비밀번호 오류 메시지
+                      Row(
+                        children: [
+                          Padding(
+                              padding:
+                              EdgeInsets.only(left: screenSize.width * 0.21)),
+                          Visibility(
+                            visible: _isErrorVisible,
+                            child: Icon(
+                              Icons.circle,
+                              size: screenSize.width * 0.01,
+                              fill: 1.0,
+                              color: Color(0xFFFFA37C),
+                            ),
+                          ),
+                          Padding(
+                              padding:
                               EdgeInsets.only(left: screenSize.width * 0.01)),
-                      Text(
-                        errorMessage,
-                        style: TextStyle(
-                            fontFamily: 'nanum',
-                            fontSize: 13,
-                            color: Color(0xFFFFA37C)),
+                          Text(
+                            errorMessage,
+                            style: TextStyle(
+                                fontFamily: 'nanum',
+                                fontSize: 13,
+                                color: Color(0xFFFFA37C)),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: screenSize.height * 0.01,
+                      ),
+                      // 입력 완료 버튼
+                      IconButton(
+                        onPressed: _pw,
+                        padding: EdgeInsets.zero,
+                        icon: Image.asset('assets/login/input_finish_btn.png',
+                            width: screenSize.width * 0.73),
                       ),
                     ],
                   ),
-                  SizedBox(
-                    height: screenSize.height * 0.01,
-                  ),
-                  // 입력 완료 버튼
-                  IconButton(
-                    onPressed: _pw,
-                    padding: EdgeInsets.zero,
-                    icon: Image.asset('assets/login/input_finish_btn.png',
-                        width: screenSize.width * 0.73),
-                  ),
+                  // 진행 표시 선
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset('assets/login/progress.png',
+                          width: screenSize.width * 0.23,
+                          height: screenSize.height * 0.01),
+                      SizedBox(
+                        width: screenSize.width * 0.02,
+                      ),
+                      Image.asset('assets/login/progress.png',
+                          width: screenSize.width * 0.23,
+                          height: screenSize.height * 0.01),
+                      SizedBox(
+                        width: screenSize.width * 0.02,
+                      ),
+                      Image.asset('assets/login/progress_colored.png',
+                          width: screenSize.width * 0.23,
+                          height: screenSize.height * 0.01),
+                    ],
+                  )
                 ],
               ),
-              // 진행 표시 선
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset('assets/login/progress.png',
-                      width: screenSize.width * 0.23,
-                      height: screenSize.height * 0.01),
-                  SizedBox(
-                    width: screenSize.width * 0.02,
-                  ),
-                  Image.asset('assets/login/progress.png',
-                      width: screenSize.width * 0.23,
-                      height: screenSize.height * 0.01),
-                  SizedBox(
-                    width: screenSize.width * 0.02,
-                  ),
-                  Image.asset('assets/login/progress_colored.png',
-                      width: screenSize.width * 0.23,
-                      height: screenSize.height * 0.01),
-                ],
-              )
             ],
           ),
-        ],
-      ),
-    ));
+        ));
   }
 
   // 비밀번호 일치 여부 확인 및 서버로 회원가입 요청
@@ -177,13 +181,15 @@ class _PasswordConfirmScreenState extends State<PasswordConfirmScreen> {
       });
 
       if (token != null) {
-        // 회원가입 및 로그인 성공 시 토큰을 LoginSplashScreen으로 전달
+        // 회원가입 및 로그인 성공 시 토큰을 Flutter Secure Storage에 저장
+        await storage.write(key: 'accessToken', value: token);
+
+        // 토큰을 LoginSplashScreen으로 전달
         Navigator.push(
           context,
           MaterialPageRoute(
             builder: (context) => LoginSplashScreen(
               nickname: widget.nickname,
-              //token: token, // 토큰 전달
             ),
           ),
         );
@@ -197,12 +203,10 @@ class _PasswordConfirmScreenState extends State<PasswordConfirmScreen> {
     }
   }
 
-  // Flutter Secure Storage 인스턴스
-  final storage = FlutterSecureStorage();
-
+  // 회원가입 및 로그인 후 토큰 반환
   Future<String?> signupAndLogin(String nickname, String password, String phoneNumber) async {
-    final signupUrl = Uri.parse('https://6a4c-182-209-67-24.ngrok-free.app/signup');
-    final loginUrl = Uri.parse('https://6a4c-182-209-67-24.ngrok-free.app/signin');
+    final signupUrl = Uri.parse('${ApiConstants.baseUrl}/signup');
+    final loginUrl = Uri.parse('${ApiConstants.baseUrl}/signin');
     final headers = {'Content-Type': 'application/json'};
 
     final signupBody = jsonEncode({
@@ -231,13 +235,9 @@ class _PasswordConfirmScreenState extends State<PasswordConfirmScreen> {
             final loginData = jsonDecode(loginResponse.body);
             print('Login Response Data: $loginData');
 
-            if (loginData['isSuccess']) {
-              final String token = loginData['result']['accessToken']; // 토큰 받기
-              return token; // 토큰 반환
-            } else {
-              print('Login failed: ${loginData['message']}');
-              return null;
-            }
+            final String token = loginData['accessToken']; // 토큰 받기
+            return token; // 토큰 반환
+
           } else {
             print('Login Server Error: ${loginResponse.statusCode}');
             print('Login Response Body: ${loginResponse.body}');
