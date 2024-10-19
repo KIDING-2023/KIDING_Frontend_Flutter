993e328333584ed1de7e124a8b4bcec27b3d55c8
siiion <siwonjeon234@gmail.com>
2024-10-19 13:23:43 +0900

    Style: 로그인 에러메세지 디자인 수정

--- lib/screen/login/start_screen.dart
+++ lib/screen/login/start_screen.dart
@@ -21,7 +21,8 @@ class StartScreen extends StatefulWidget {
 class _StartScreenState extends State<StartScreen> {
   final _nicknameController = TextEditingController(); // 닉네임 컨트롤러
   final _passwordController = TextEditingController(); // 비밀번호 컨트롤러
-  bool _isErrorVisible = false; // 로그인 에러 발생 체크
+  bool errorVisible = false; // 에러 메시지 가시성
+  String errorMessage = ""; // 에러 메시지
   bool _isStayLoggedIn = false; // 로그인 상태 유지 체크
   final _storage = FlutterSecureStorage(); // Secure storage 객체 생성
 
@@ -55,9 +56,8 @@ class _StartScreenState extends State<StartScreen> {
                         filled: true,
                         fillColor: Color(0xfff6f6f6),
                         border: OutlineInputBorder(
-                          borderSide: BorderSide.none,
-                          borderRadius: BorderRadius.circular(100)
-                        ),
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(100)),
                         contentPadding: EdgeInsets.all(20)),
                     style: TextStyle(
                       fontFamily: 'nanum',
@@ -83,9 +83,8 @@ class _StartScreenState extends State<StartScreen> {
                         filled: true,
                         fillColor: Color(0xfff6f6f6),
                         border: OutlineInputBorder(
-                          borderSide: BorderSide.none,
-                          borderRadius: BorderRadius.circular(100)
-                        ),
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(100)),
                         contentPadding: EdgeInsets.all(20)),
                     style: TextStyle(
                       fontFamily: 'nanum',
@@ -95,6 +94,36 @@ class _StartScreenState extends State<StartScreen> {
                   ),
                 ),
                 // 비밀번호 입력칸과 시작하기 버튼 사이의 여백
+                SizedBox(
+                  height: screenSize.height * 0.01,
+                ),
+                // 에러메세지
+                Row(
+                  children: [
+                    Padding(
+                        padding:
+                            EdgeInsets.only(left: screenSize.width * 0.21)),
+                    Visibility(
+                      visible: errorVisible,
+                      child: Icon(
+                        Icons.circle,
+                        size: screenSize.width * 0.01,
+                        fill: 1.0,
+                        color: Color(0xFFFFA37C),
+                      ),
+                    ),
+                    Padding(
+                        padding:
+                            EdgeInsets.only(left: screenSize.width * 0.01)),
+                    Text(
+                      errorMessage,
+                      style: TextStyle(
+                          fontFamily: 'nanum',
+                          fontSize: 13,
+                          color: Color(0xFFFFA37C)),
+                    ),
+                  ],
+                ),
                 SizedBox(
                   height: screenSize.height * 0.01,
                 ),
@@ -117,14 +146,16 @@ class _StartScreenState extends State<StartScreen> {
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
                     GestureDetector(
-                      onTap: /*_toggleLoginState*/ () {},
+                      onTap: () {
+                        _toggleLoginState();
+                      },
                       child: Image.asset(
                         _isStayLoggedIn
                             ? 'assets/login/keep_login_selected_btn.png' // 로그인 상태 유지 활성화 이미지
                             : 'assets/login/keep_login_btn.png',
                         // 로그인 상태 유지 비활성화 이미지
-                        width: 75,
-                        height: 18,
+                        width: 100,
+                        height: 22,
                       ),
                     ),
                     // 체크박스와 '로그인 유지' 텍스트 사이의 여백
@@ -137,13 +168,6 @@ class _StartScreenState extends State<StartScreen> {
                 SizedBox(
                   height: screenSize.height * 0.01,
                 ),
-                // 에러 메세지
-                Container(
-                    child: _isErrorVisible
-                        ? Image.asset('assets/login/start_error_text.png',
-                            width: screenSize.width * 0.52,
-                            height: screenSize.height * 0.05)
-                        : SizedBox()),
               ],
             ),
             // 회원가입하기 | 닉네임 찾기 | 비밀번호 찾기
@@ -208,12 +232,6 @@ class _StartScreenState extends State<StartScreen> {
     ));
   }
 
-  // // 로그인 임시
-  // void _login() {
-  //   Navigator.push(
-  //       context, MaterialPageRoute(builder: (context) => HomeScreen()));
-  // }
-
   // 로그인 유지 버튼 토글
   void _toggleLoginState() {
     setState(() {
@@ -239,16 +257,19 @@ class _StartScreenState extends State<StartScreen> {
 
       // 서버 응답 데이터
       var responseData = json.decode(response.body);
-      print("서버 응답 데이터: $responseData");  // 서버 응답 로그 출력
+      print("서버 응답 데이터: $responseData"); // 서버 응답 로그 출력
 
       // 로그인 성공 여부를 토큰의 유무로 판단
-      if (responseData['accessToken'] != null && responseData['refreshToken'] != null) {
+      if (responseData['accessToken'] != null &&
+          responseData['refreshToken'] != null) {
         // 로그인 상태 유지 옵션이 선택된 경우 토큰 저장
         if (_isStayLoggedIn) {
           await _storage.write(key: 'isLoggedIn', value: 'true');
           await _storage.write(key: 'nickname', value: nickname);
-          await _storage.write(key: 'accessToken', value: responseData['accessToken']);
-          await _storage.write(key: 'refreshToken', value: responseData['refreshToken']);
+          await _storage.write(
+              key: 'accessToken', value: responseData['accessToken']);
+          await _storage.write(
+              key: 'refreshToken', value: responseData['refreshToken']);
         }
 
         // 홈 화면으로 이동
@@ -257,9 +278,17 @@ class _StartScreenState extends State<StartScreen> {
       } else {
         // 토큰이 없으면 로그인 실패 처리
         print("로그인 실패: 서버에서 토큰을 반환하지 않았습니다.");
-        setState(() {
-          _isErrorVisible = true;  // 에러 메시지 표시
-        });
+        if (responseData['result'] == "가입된 닉네임이 아닙니다.") {
+          setState(() {
+            errorVisible = true; // 에러 메시지 표시
+            errorMessage = "없는 닉네임입니다.";
+          });
+        } else {
+          setState(() {
+            errorVisible = true; // 에러 메시지 표시
+            errorMessage = "비밀번호가 틀립니다.";
+          });
+        }
       }
     } catch (error) {
       // 에러가 발생한 경우, 에러 로그 출력
@@ -274,7 +303,8 @@ class _StartScreenState extends State<StartScreen> {
       }
 
       setState(() {
-        _isErrorVisible = true;  // 에러 메시지 표시
+        errorVisible = true; // 에러 메시지 표시
+        errorMessage = "로그인에 실패했습니다.";
       });
     }
   }
