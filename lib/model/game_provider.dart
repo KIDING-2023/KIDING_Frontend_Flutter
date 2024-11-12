26ad25da17dbf09c7d47ec9b7bda5441bfc3894e
siiion <siwonjeon234@gmail.com>
2024-11-12 21:00:29 +0900

    Chore: game_provider 수정

--- lib/model/game_provider.dart
+++ lib/model/game_provider.dart
@@ -6,32 +6,64 @@ class Player {
   int chips;
 
   Player({required this.playerNum, this.position = 0, this.chips = 0});
+
+  // JSON 직렬화 및 비직렬화 기능 추가
+  Map<String, dynamic> toJson() => {
+    'playerNum': playerNum,
+    'position': position,
+    'chips': chips,
+  };
+
+  factory Player.fromJson(Map<String, dynamic> json) => Player(
+    playerNum: json['playerNum'],
+    position: json['position'],
+    chips: json['chips'],
+  );
 }
 
 class GameProvider extends ChangeNotifier {
-  List<Player> players = [];
-  int currentPlayerIndex = 0;
+  List<Player> _players = [];
+  int _currentPlayerIndex = 0;
+
+  // 불변성 유지: 외부에서는 읽기만 가능
+  List<Player> get players => List.unmodifiable(_players);
+
+  // 현재 플레이어 가져오기
+  Player get currentPlayer => _players[_currentPlayerIndex];
 
   void setPlayers(int count) {
-    players = List.generate(count, (index) => Player(playerNum: index + 1));
-    currentPlayerIndex = 0;
+    _players = List.generate(count, (index) => Player(playerNum: index + 1));
+    _currentPlayerIndex = 0;
     notifyListeners();
   }
 
-  Player get currentPlayer => players[currentPlayerIndex];
-
   void updatePlayerPosition(int steps) {
-    currentPlayer.position += steps;
-    notifyListeners();
+    if (_players.isNotEmpty) {
+      currentPlayer.position += steps;
+      notifyListeners();
+    }
   }
 
   void updatePlayerChips(int count) {
-    currentPlayer.chips += count;
-    notifyListeners();
+    if (_players.isNotEmpty) {
+      currentPlayer.chips += count;
+      notifyListeners();
+    }
   }
 
   void nextPlayerTurn() {
-    currentPlayerIndex = (currentPlayerIndex + 1) % players.length;
+    if (_players.isNotEmpty) {
+      _currentPlayerIndex = (_currentPlayerIndex + 1) % _players.length;
+      notifyListeners();
+    }
+  }
+
+  void resetGame() {
+    for (var player in _players) {
+      player.position = 0;
+      player.chips = 0;
+    }
+    _currentPlayerIndex = 0;
     notifyListeners();
   }
 }
