f74eb54e3e8894af9f37d416e0d1a94704acd047
siiion <siwonjeon234@gmail.com>
2024-11-12 21:04:46 +0900

    Chore: 친구 요청 화면 수정

--- lib/screen/friends/friends_request_screen.dart
+++ lib/screen/friends/friends_request_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../mypage/mypage_screen.dart';
 
 class FriendsRequestScreen extends StatefulWidget {
@@ -10,7 +9,7 @@ class FriendsRequestScreen extends StatefulWidget {
 }
 
 class _FriendsRequestScreenState extends State<FriendsRequestScreen> {
-  List<Map<String, String>> friendRequests = [
+  final List<Map<String, String>> friendRequests = [
     {'name': 'OOO 친구', 'rank': '랭킹 1위'},
     {'name': 'OOO 친구', 'rank': '랭킹 3위'},
     {'name': 'OOO 친구', 'rank': '랭킹 5위'},
@@ -22,158 +21,179 @@ class _FriendsRequestScreenState extends State<FriendsRequestScreen> {
     final screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-        backgroundColor: Color(0xFFe7e7e7),
-        body: Stack(
-          children: [
-            Positioned(
-              top: screenHeight * 0.0375,
-              left: screenWidth * 0.0417,
-              child: Padding(
-                  padding: const EdgeInsets.only(top: 15),
-                  child: IconButton(
-                    icon: Image.asset('assets/kikisday/kikisday_back_btn.png',
-                        width: screenWidth * 0.0366,
-                        height: screenHeight * 0.025),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  )),
+      backgroundColor: const Color(0xFFe7e7e7),
+      body: Stack(
+        children: [
+          // 뒤로 가기 버튼
+          Positioned(
+            top: screenHeight * 0.0375,
+            left: screenWidth * 0.0417,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 15),
+              child: IconButton(
+                icon: Image.asset(
+                  'assets/kikisday/kikisday_back_btn.png',
+                  width: screenWidth * 0.0366,
+                  height: screenHeight * 0.025,
+                ),
+                onPressed: () => Navigator.pop(context),
+              ),
             ),
-            Positioned(
-                top: screenHeight * 0.13,
-                left: screenWidth * 0.0831,
-                child: Text(
-                  '친구요청',
-                  style: TextStyle(
-                      fontSize: 15.79,
-                      fontFamily: 'Nanum',
-                      color: Colors.black),
-                )),
-            Positioned(
-                top: screenHeight * 0.15,
-                left: 0,
-                right: 0,
-                child: SizedBox(
-                  height: screenHeight * 0.65,
-                  child: ListView.builder(
-                      itemCount: friendRequests.length,
-                      itemBuilder: (context, index) {
-                        return _buildFriendRequestCard(
-                            friendRequests[index]['name']!,
-                            friendRequests[index]['rank']!);
-                      }),
-                )),
-            // 하단바
-            Positioned(
-                top: screenHeight * 0.91,
-                left: 0,
-                right: 0,
-                child: Container(
-                  height: screenHeight * 0.09,
-                  color: Colors.white,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: <Widget>[
-                      IconButton(
-                        icon: Image.asset(
-                          'assets/home/ranking_unselected.png',
-                          width: screenWidth * 0.1,
-                          height: screenHeight * 0.04,
-                        ),
-                        onPressed: () {},
-                      ),
-                      IconButton(
-                        icon: Image.asset(
-                          'assets/home/home_selected.png',
-                          width: screenWidth * 0.1,
-                          height: screenHeight * 0.04,
-                        ),
-                        onPressed: () {},
-                      ),
-                      IconButton(
-                        icon: Image.asset(
-                          'assets/home/mypage_unselected.png',
-                          width: screenWidth * 0.1,
-                          height: screenHeight * 0.04,
-                        ),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => MyPageScreen()),
-                          );
-                        },
-                      ),
-                    ],
+          ),
+          // 친구 요청 타이틀
+          Positioned(
+            top: screenHeight * 0.13,
+            left: screenWidth * 0.0831,
+            child: const Text(
+              '친구요청',
+              style: TextStyle(
+                fontSize: 15.79,
+                fontFamily: 'Nanum',
+                color: Colors.black,
+              ),
+            ),
+          ),
+          // 친구 요청 목록
+          Positioned(
+            top: screenHeight * 0.15,
+            left: 0,
+            right: 0,
+            child: SizedBox(
+              height: screenHeight * 0.65,
+              child: ListView.builder(
+                itemCount: friendRequests.length,
+                itemBuilder: (context, index) {
+                  return _buildFriendRequestCard(
+                    friendRequests[index]['name']!,
+                    friendRequests[index]['rank']!,
+                    screenWidth,
+                    screenHeight,
+                  );
+                },
+              ),
+            ),
+          ),
+          // 하단바
+          Positioned(
+            top: screenHeight * 0.91,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: screenHeight * 0.09,
+              color: Colors.white,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                    icon: Image.asset(
+                      'assets/home/ranking_unselected.png',
+                      width: screenWidth * 0.1,
+                      height: screenHeight * 0.04,
+                    ),
+                    onPressed: () {},
                   ),
-                ))
-          ],
-        ));
+                  IconButton(
+                    icon: Image.asset(
+                      'assets/home/home_selected.png',
+                      width: screenWidth * 0.1,
+                      height: screenHeight * 0.04,
+                    ),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: Image.asset(
+                      'assets/home/mypage_unselected.png',
+                      width: screenWidth * 0.1,
+                      height: screenHeight * 0.04,
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const MyPageScreen(),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
-  Widget _buildFriendRequestCard(String name, String rank) {
-    final screenWidth = MediaQuery.of(context).size.width;
-    final screenHeight = MediaQuery.of(context).size.height;
-
+  Widget _buildFriendRequestCard(String name, String rank, double screenWidth, double screenHeight) {
     return Container(
-      margin: const EdgeInsets.only(bottom: 15, left: 30, right: 30),
+      margin: const EdgeInsets.symmetric(vertical: 7.5, horizontal: 30),
       width: screenWidth * 0.8332,
       height: screenHeight * 0.1602,
       decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('assets/friends/request_box.png'),
-              fit: BoxFit.cover)),
+        image: const DecorationImage(
+          image: AssetImage('assets/friends/request_box.png'),
+          fit: BoxFit.cover,
+        ),
+      ),
       child: Stack(
         children: [
           Positioned(
-              top: screenHeight * 0.0137,
-              left: screenWidth * 0.0309,
-              child: Image.asset(
-                'assets/friends/icon.png',
-                width: screenWidth * 0.20794,
-                height: screenHeight * 0.0936,
-              )),
+            top: screenHeight * 0.0137,
+            left: screenWidth * 0.0309,
+            child: Image.asset(
+              'assets/friends/icon.png',
+              width: screenWidth * 0.20794,
+              height: screenHeight * 0.0936,
+            ),
+          ),
           Positioned(
-              top: screenHeight * 0.03,
-              left: screenWidth * 0.277,
-              child: Text(
-                name,
-                style: TextStyle(
-                    fontFamily: 'Nanum', fontSize: 17, color: Colors.black),
-              )),
+            top: screenHeight * 0.03,
+            left: screenWidth * 0.277,
+            child: Text(
+              name,
+              style: const TextStyle(
+                fontFamily: 'Nanum',
+                fontSize: 17,
+                color: Colors.black,
+              ),
+            ),
+          ),
           Positioned(
-              top: screenHeight * 0.063,
-              left: screenWidth * 0.277,
-              child: Text(
-                rank,
-                style: TextStyle(
-                    fontFamily: 'Nanum',
-                    fontSize: 12,
-                    color: Color(0xFFb3b3b3)),
-              )),
+            top: screenHeight * 0.063,
+            left: screenWidth * 0.277,
+            child: Text(
+              rank,
+              style: const TextStyle(
+                fontFamily: 'Nanum',
+                fontSize: 12,
+                color: Color(0xFFb3b3b3),
+              ),
+            ),
+          ),
           Positioned(
-              top: screenHeight * 0.1,
-              left: 0,
-              right: 0,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: Image.asset(
-                      'assets/friends/reject_btn.png',
-                      width: screenWidth * 0.35,
-                    ),
+            top: screenHeight * 0.1,
+            left: 0,
+            right: 0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Image.asset(
+                    'assets/friends/reject_btn.png',
+                    width: screenWidth * 0.35,
                   ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Image.asset(
-                      'assets/friends/accept_btn.png',
-                      width: screenWidth * 0.35,
-                    ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Image.asset(
+                    'assets/friends/accept_btn.png',
+                    width: screenWidth * 0.35,
                   ),
-                ],
-              ))
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
